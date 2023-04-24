17144a694f0450bb3d84b06309c469ea991c42d6
Anirudh-idk <aniragg30@gmail.com>
2023-04-24 20:14:16 +0530

    modularized,added documentation

--- src/converter.py
+++ src/converter.py
@@ -1,29 +1,47 @@
-import pdfplumber
-import polars as pl
+import pdfplumber  # https://github.com/jsvine/pdfplumber
+import polars as pl  # https://www.pola.rs/
 
-file = ""  # input pdf filepath
+
+def FormattedTable(table):
+    """
+    Checks if the table is empty
+    -----------------------------------------------------------------------
+    Formats the table to:
+    1) remove column headers
+    2) replaces \\n with whitespace to avoid formatting issues in final csv
+            e.g.  10/07\\n9:30 - 11:00AM  -->    10/07   9:30 - 11:00AM
+
+    """
+    if table:
+        table = table[2:]
+        for item in table:
+            if "\n" in item[-2]:
+                item[-2] = item[-2].replace("\n", "  ")
+                return table
+    else:
+        return []
+
+
+def ExtractFromMultiplePages(pages):
+    """
+
+    Takes a list of pdfplumber.page objects as input then
+    Iterates through the list of pages extracting tables on each page
+    and adding it to the final dataframe to be converted into csv file
+
+    """
+    df = pl.DataFrame()
+    for page in pages:
+        table = page.extract_table()
+        table = FormattedTable(table)
+        df = df.vstack(pl.DataFrame(table, orient="row"))
+    return df
+
+
+file = r""  # input pdf filepath
 pdf = pdfplumber.open(file)
-
-
 pages = pdf.pages[:]  # pages to be converted
 
-
-data = pl.DataFrame()
-
-for page in pages:  # iterating through the list of pages
-    table = page.extract_table()
-    if table:
-        table = table[2:]  # removing columns headers
-    else:
-        continue
-    for (
-        item
-    ) in table:  # replacing \n with spaces to avoid formatting issues in csv files
-        if "\n" in item[-2]:
-            item[-2] = item[-2].replace("\n", "  ")
-    data = data.vstack(
-        pl.DataFrame(table, orient="row")
-    )  # extending dataframes with data from each page
-
+data = ExtractFromMultiplePages(pages)
 
 data.write_csv(file.split(".")[0] + ".csv")
