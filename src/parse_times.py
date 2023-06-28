87e98d62580112da0de6591256501ed411499a2a
majimearun <arunachala0619@gmail.com>
2023-06-28 20:48:58 +0530

    feat: finish funstions to parse exam times

--- src/parse_times.py
+++ src/parse_times.py
@@ -1,18 +1,18 @@
 import pandas as pd
 
 
-def parse_midsem_time(midsem_time: str, year: int) -> str:
+def parse_time(time: str, year: int) -> str:
     """
-    Function to parse the midsem time from the string given by ttd, to ISO format for easier consumption in the web site.
+    Function to parse the time from the string given by ttd, to ISO format for easier consumption in the web site.
 
     Args:
-        midsem_time (str): The string given by ttd for the midsem time. (Format: "13/03 11.30 - 1.00PM")
-        year (int): The ayear of the timetable. (example: 2021)
+        time (str): The string given by ttd for the time. (Format: "13/03 11.30 - 1.00PM")
+        year (int): The year of the timetable. (example: 2023)
 
     Returns:
-        str: The midsem time in ISO format. (example: "2021-03-13T11:30:00|2021-03-13T13:00:00")
+        str: The time in ISO format. (example: "2023-03-13T11:30:00|2023-03-13T13:00:00")
     """
-    parts = midsem_time.split()
+    parts = time.split()
 
     date_in_year = parts[0].split("/")
 
@@ -27,9 +27,19 @@ def parse_midsem_time(midsem_time: str, year: int) -> str:
     start_pm = start.copy()
 
     if end_time == "PM":
-        end[0] = str(int(end[0]) + 12)
+        # as hours are from 0 to 23, we add 12 to the hours to get the correct time
+        if end[0] != "12":
+            end[0] = str(int(end[0]) + 12)
         start_pm[0] = str(int(start_pm[0]) + 12)
 
+    # mostly unnecessary, but for the sake of completeness
+    if end_time == "AM" and end[0] == "12":
+        end[0] = "00"
+
+    # consider both cases if end time is PM
+    # assumption is exam doesn't last longer than 12 hours (sane)
+    # so if end time is PM and start_pm is less than end, then start_pm is correct start
+    # otherwise start/start_am is correct start
     start_date_am = date + "T" + start[0] + ":" + start[1] + ":00"
     start_date_pm = date + "T" + start_pm[0] + ":" + start_pm[1] + ":00"
 
@@ -56,3 +66,24 @@ def parse_midsem_time(midsem_time: str, year: int) -> str:
     else:
         start_date_am = pd.Timestamp(start_date_am) - pd.Timedelta(hours=5, minutes=30)
         return str(start_date_am.isoformat()) + "|" + str(end_date.isoformat())
+
+
+def parse_compre_time(compre_time: str, year: int):
+    """
+    Function to parse the compre time from the string given by ttd, to ISO format for easier consumption in the web site.
+
+    Args:
+        compre_time (str): The string given by ttd for the compre time. (Format: "19/05 FN")
+        year (int): The year of the timetable. (example: 2023)
+
+    Returns:
+        str: The compre time in ISO format. (example: "2023-05-19T09:30:00|2023-05-19T12:30:00")
+    """
+    lookup = {"FN": "9.30 - 12.30PM", "AN": "2.00 - 5.00PM"}
+    time = compre_time.split()[0] + " " + lookup[compre_time.split()[1]]
+    return parse_time(time, year)
+
+
+if __name__ == "__main__":
+    print(parse_time("13/03 11.30 - 1.00PM", 2023))
+    print(parse_compre_time("19/05 FN", 2023))
